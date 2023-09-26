28f85f8be7abf8c759664a482503f00a40939420
Xavi661 <javierpadin661@gmail.com>
2023-09-25 23:15:55 -0500

    boton cambia camara

--- src/Camara_scanner/QrScanner.js
+++ src/Camara_scanner/QrScanner.js
@@ -4,10 +4,10 @@ import QrReader from 'react-qr-scanner';
 function QRScanner() {
   const [result, setResult] = useState('');
   const [facingMode, setFacingMode] = useState('environment'); // Por defecto, usa la cámara trasera
-
   const handleScan = (data) => {
     if (data) {
-      setResult(data);
+      console.log(data); // Muestra los datos del escaneo en la consola
+      setResult(data.text); // Actualiza el estado con el texto del escaneo
     }
   };
 
@@ -16,19 +16,55 @@ function QRScanner() {
     setFacingMode(facingMode === 'user' ? 'environment' : 'user');
   };
 
+
   return (
     <div>
+      
       <button onClick={toggleCamera}>Cambiar Cámara</button>
+
       <QrReader
         delay={300}
         onError={(error) => console.log(error)}
         onScan={handleScan}
         style={{ width: '100%' }}
-        facingMode={facingMode} // Establece la cámara
       />
-      <p>Resultado: {result}</p>
+      <p>Resultado: <a href={result} target='_blank'>{result}</a></p>
     </div>
   );
 }
-
 export default QRScanner;
+
+// import React, { useState, useEffect } from 'react';
+// import QrReader from 'react-qr-scanner';
+
+// function QRScanner() {
+//   const [result, setResult] = useState('');
+//   const [facingMode, setFacingMode] = useState('environment'); // Por defecto, usa la cámara trasera
+
+//   const handleScan = (data) => {
+//     if (data) {
+//       setResult(data);
+//     }
+//   };
+
+//   const toggleCamera = () => {
+//     // Cambia entre la cámara trasera y la cámara frontal
+//     setFacingMode(facingMode === 'user' ? 'environment' : 'user');
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={toggleCamera}>Cambiar Cámara</button>
+//       <QrReader
+//         delay={300}
+//         onError={(error) => console.log(error)}
+//         onScan={handleScan}
+//         style={{ width: '100%' }}
+//         facingMode={facingMode} // Establece la cámara
+//       />
+//       <p>Resultado: {result}</p>
+//     </div>
+//   );
+// }
+
+// export default QRScanner;
